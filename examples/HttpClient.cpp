698bbe35360b68fa21801978a4e5c393b37bb228
dyu <david.yu.ftw@gmail.com>
2018-02-21 19:05:45 +0800

    use signal

--- examples/HttpClient.cpp
+++ examples/HttpClient.cpp
@@ -2,11 +2,47 @@
 #include <string>
 #include <mutex>
 #include <condition_variable>
+#include <csignal>
 
 #include <brynet/net/SocketLibFunction.h>
 #include <brynet/net/http/HttpService.h>
 #include <brynet/net/http/HttpFormat.h>
 #include <brynet/net/http/WebSocketFormat.h>
+
+struct Signal
+{
+    sig_atomic_t count;
+    std::mutex mutex;
+    std::condition_variable cv;
+    
+    Signal(int val = 0) : count(val) {}
+    Signal& set(int val)
+    {
+        count = val;
+        return *this;
+    }
+    bool notify()
+    {
+        bool notify = 0 == --count;
+        if (notify)
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            lock.unlock();
+            cv.notify_one();
+        }
+        return notify;
+    }
+    void wait()
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        cv.wait(lock, [this]{return 1 > count;});
+    }
+    
+    // no copy
+private:
+    Signal(const Signal&) = delete;
+    const Signal& operator=(const Signal&) = delete;
+};
 
 static const char DEFAULT_HOST[] = "127.0.0.1";
 static const int DEFAULT_PORT = 8080;
@@ -44,8 +80,8 @@ int main(int argc, char* argv[])
     const char* host = resolveIpPort(argc > 1 ? argv[1] : nullptr, &port);
     const char* uri = argc > 2 ? argv[2] : "/";
     
-    auto service = std::make_shared<brynet::net::WrapTcpService>();
-    service->startWorkThread(2);
+    brynet::net::WrapTcpService service;
+    service.startWorkThread(2);
     
     //fprintf(stdout, "Connecting to http://%s:%d%s\n", host, port, uri);
     
@@ -56,14 +92,17 @@ int main(int argc, char* argv[])
         return 1;
     }
     
+    Signal signal(1);
+    
     auto socket = brynet::net::TcpSocket::Create(fd, false);
-    service->addSession(std::move(socket), [uri](const brynet::net::TCPSession::PTR& tcpSession) {
+    
+    service.addSession(std::move(socket), [&signal, uri](const brynet::net::TCPSession::PTR& tcpSession) {
         //fprintf(stdout, "Connected.\n");
-        brynet::net::HttpService::setup(tcpSession, [uri](const brynet::net::HttpSession::PTR& httpSession) {
-            httpSession->setHttpCallback([](const brynet::net::HTTPParser& httpParser, const brynet::net::HttpSession::PTR& session) {
+        brynet::net::HttpService::setup(tcpSession, [&signal, uri](const brynet::net::HttpSession::PTR& httpSession) {
+            httpSession->setHttpCallback([&signal](const brynet::net::HTTPParser& httpParser, const brynet::net::HttpSession::PTR& session) {
                 //http response handle
                 std::cout << httpParser.getBody() << std::endl;
-                exit(0);
+                signal.notify();
             });
             
             brynet::net::HttpRequest req;
@@ -75,12 +114,6 @@ int main(int argc, char* argv[])
         });
     }, false, nullptr, 1024 * 1024, false);
     
-    std::cin.get();
-    //brynet::net::EventLoop mainLoop;
-    //while (true)
-    //{
-    //    mainLoop.loop(1000);
-    //}
-    
+    signal.wait();
     return 0;
 }
